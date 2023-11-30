accc0b2b0e4dd70cec90d82525b5491a789fc937
nerogude <ogudenero@gmail.com>
2023-11-30 15:24:49 +0000

    Welcome page

--- src/test/java/pageObjects/WelcomePage.java
+++ src/test/java/pageObjects/WelcomePage.java
@@ -1,5 +1,25 @@
 package pageObjects;
 
-public class WelcomePage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class WelcomePage extends BasePage{
+
+	public WelcomePage(WebDriver driver) {
+		super(driver);
+	}
+	@FindBy(css="body > div:nth-child(8) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1)")
+	WebElement lagosCheckbox;
+	
+	@FindBy(css=".btn-primary.fw-800.w-full.py-2")
+	WebElement confirmCityButton;
+	
+	public void clickLagosCheckbox() {
+		lagosCheckbox.click();
+	}
+	public void clickConfirmCity() {
+		confirmCityButton.click();
+	}
 
 }
